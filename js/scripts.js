f98f201b3d144cd30109196bf899fa64d4103c83
Min Chang <minchangmhc@gmail.com>
2021-03-30 16:06:00 -0700

    Add utility functions to make code look cleaner.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+// -----Utility Logic-----
+function noInputtedWord(word, text) {
+  return ((text.trim().length === 0 || (word.trim().length === 0)));
+}
 // -----Business Logic-----
 function wordCounter(text) {
   if (text.trim().length === 0) {
@@ -15,7 +19,7 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
-  if (text.trim().length === 0 || (word.trim().length === 0)) {
+  if (noInputtedWord(word, text)) {
     return 0;
   }
   const wordArray = text.split(" ");
@@ -29,7 +33,22 @@ function numberOfOccurrencesInText(word, text) {
 }
 
 function boldPassage(word, text) {
-  return "<p>" + text + "</p>";
+  if (noInputtedWord(word, text)) {
+    return "";
+  }
+  let htmlString = "<p>"
+  let textArray = text.split(" ")
+  textArray.forEach(function (element, index) {
+    if (word === element) {
+      htmlString = htmlString.concat("<b>" + element + "</b>");
+    } else {
+      htmlString = htmlString.concat(element);
+    }
+    if (index !== (textArray.length - 1)) {
+      htmlString = htmlString.concat(" ");
+    }
+  });
+  return htmlString + "</p>";
 }
 
 
@@ -47,5 +66,6 @@ $(document).ready(function () {
     const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
     $("#total-count").html(wordCount);
     $("#selected-count").html(occurrencesOfWord);
+    $("#bolded-passage").html(boldPassage(word, passage));
   });
 });
